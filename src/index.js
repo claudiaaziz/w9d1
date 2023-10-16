7429e252dcc295cebf68c01247c6545fb2da13b9
claudiaaziz <claudiahannahh@gmail.com>
2023-10-16 16:17:23 -0400

    add draw and move method

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
 
 import MovingObject from "./moving_object";
 window.MovingObject = MovingObject
+
+const game = document.getElementById("game-canvas");
+const ctx = game.getContext("2d")
+
+  const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00",
+  });
+
+  window.mo
+  // window.mo.draw(ctx);
--- src/moving_object.js
+++ src/moving_object.js
@@ -1,9 +1,19 @@
 class MovingObject {
-  constructor () {
-    this.pos = pos
-    this.vel = vel
-    this.radius = radius
-    this.color = color
+  constructor (obj) {
+    this.pos = obj.pos
+    this.vel = obj.vel
+    this.radius = obj.radius
+    this.color = obj.color
+  }
+
+  // draw(ctx) {
+  //   ctx.beginPath()
+  //   ctx.arc(100, 75, 50, 0 , 2 * Math.PI)
+  //   ctx.stroke()
+  // }
+
+  move() {
+    return this.pos += this.vel
   }
 }
 
@@ -15,3 +25,4 @@ class MovingObject {
   // }
 
 export default MovingObject;
+
